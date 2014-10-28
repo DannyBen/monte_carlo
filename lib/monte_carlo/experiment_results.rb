60b79ee007684a2f023d6e082bda278d39010bb8
Assaf Gelber <assaf.gelber@gmail.com>
2014-10-28 07:36:31 +0200

    Add memoizations and 'of' methods

--- lib/monte_carlo/experiment_results.rb
+++ lib/monte_carlo/experiment_results.rb
@@ -1,12 +1,6 @@
-require 'forwardable'
-
 module MonteCarlo
   class ExperimentResults
     include Enumerable
-    extend Forwardable
-
-    attr_reader :results
-    def_delegators :@results, :size, :[], :<<, :count, :each, :uniq
 
     def initialize
       @results = []
@@ -18,13 +12,42 @@ module MonteCarlo
       end
     end
 
+    def << result
+      @results << result
+      reset_memoizers
+    end
+
+    def size
+      @results.size
+    end
+
     def probability_distribution
-      @results.group_by(&:value).inject({}) { |percentages, (value, results)| percentages[value] = results.size.to_f / self.size; percentages }
+      @probability_distribution ||= @results.group_by(&:value).inject({}) do |probabilites, (value, results)|
+        probabilites[value] = results.size.to_f / self.size
+        probabilites
+      end
     end
 
     def frequency_distribution
-      @results.group_by(&:value).inject({}) { |counts, (value, results)| counts[value] = results.size; counts }
+      @frequency_distribution ||= @results.group_by(&:value).inject({}) do |frequencies, (value, results)|
+        frequencies[value] = results.size
+        frequencies
+      end
     end
 
+    def probability_of(value)
+      probability_distribution.fetch(value, 0)
+    end
+
+    def frequency_of(value)
+      frequency_distribution.fetch(value, 0)
+    end
+
+    private
+
+    def reset_memoizers
+      @probability_distribution = nil
+      @frequency_distribution = nil
+    end
   end
 end
--- spec/lib/monte_carlo/experiment_results_spec.rb
+++ spec/lib/monte_carlo/experiment_results_spec.rb
@@ -11,6 +11,12 @@ describe MonteCarlo::ExperimentResults do
     results
   end
 
+  shared_examples_for :of_methdds do |method|
+    it 'returns 0 when the value does not exist' do
+      expect(results.send(method, 'does not exist')).to eq 0
+    end
+  end
+
   describe :probability_distribution do
     it 'should return the correct probability distribution for the results' do
       expect(results.probability_distribution.keys).to contain_exactly(true, false)
@@ -24,6 +30,22 @@ describe MonteCarlo::ExperimentResults do
       expect(results.frequency_distribution.keys).to contain_exactly(true, false)
       expect(results.frequency_distribution[true]).to eq 33
       expect(results.frequency_distribution[false]).to eq 66
+    end
+  end
+
+  describe :probability_of do
+    it_behaves_like :of_methdds, :probability_of
+
+    it 'should return the correct probability of a value' do
+      expect(results.probability_of(true)).to eq 1 / 3.0
+    end
+  end
+
+  describe :frequency_of do
+    it_behaves_like :of_methdds, :frequency_of
+
+    it 'should return the correct frequency of a value' do
+      expect(results.frequency_of(true)).to eq 33
     end
   end
 
